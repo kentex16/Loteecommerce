1e02f367706d987c781f4a3411aca3b8723d95c7
ADMIN <kenneth.novero@cvsu.edu.ph>
2023-09-28 18:17:26 +0800

    Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,83 +1,271 @@
-<style>
-    .login-container {
-        background: rgba(0, 0, 0, 0.3); 
-        padding: 20px; 
-        border-radius: 1px; 
-        width: 600px;
-        min-height: 300px; 
-        margin: 0 auto; 
-        text-align: center; 
-    }
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+        *
+        {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Poppins',sans-serif;
+        }
+        .header{
+            position: fixed;
+            width: 100%;
+            height: 60px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 20px 100px;
+            z-index: 99;
+            background: transparent;
+        }
+        .header .nav a{
+            text-decoration: none;
+            color: #fff;
+            font-size: 17px;
+            margin-left: 20px;
+            font-weight: 600;
+        }
+        .header .search input {
+            background: transparent;
+            font-size: 20px;
+            border-radius: 5px;
+            border: 2px solid white;
+            padding-left: 10px;
+            right: 100px;
+            outline: none;
+            color: #fff;
+        }
+        .header .search i{
+            position: absolute;
+            right: 110px;
+            top: 22px;
+            color: #fff;
+        }
+        .background
+        {
+            width: 100%;
+            height: 100vh;
+            background-image: url('/images/your-background-image.jpg');
+            background-position: center;
+            background-size: cover;
+        }
+        .home{
+            position: absolute;;
+            top: 50%;
+            left: 50%;
+            width: 75%;
+            height: 75%;
+            transform: translate(-50%,-50%);
+            background-image: url('/images/your-background-image.jpg');
+            background-position: center;
+            background-size: cover;
+            display: flex;
+            margin-top: 10px;
+            border: 1px solid black;
+            border-radius: 10px;
+            border: none;
+        }
+        .content{
+            display: flex;
+            flex-direction: column;
+            width: 700px;
+            padding: 100px 0;
+        }
+        .content a{
+            position: relative;
+            text-decoration: none;
+            color: #fff;
+            font-size: 3em;
+            font-weight: 700;
+            top: -40px;
+            left: 80px;
+        }
+        .content h2,
+        .content h3,
+        .content pre {
+            text-align: center;
+            color: #fff;
+            display: block;
+        }
 
-    .background-image {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-image: url('/images/your-background-image.jpg'); 
-        background-size: cover;
-        filter: blur(5px); 
-        z-index: -1; 
-    }
+        .content h2 {
+            font-size: 3.5em;
+        }
 
-    .authentication-card {
-        z-index: 1; 
-        /* Add your card styles here */
-    }
-</style>
+        .content h3 {
+            font-size: 2em;
+        }
 
+        .content pre {
+            margin-top: 20px;
+            font-size: 1em;
+        }
 
-<x-guest-layout>
-    <div class="login-container">
-        <div class="background-image"></div>
-        <x-authentication-card>
-            <x-slot name="logo">
-                <x-authentication-card-logo />
-            </x-slot>
+        .content .icon {
+            margin-top:20px;
+            font-size: 1.5em;
+            display: flex;
+            justify-content: center;
+        }
+        .content .icon i {
+            margin-left: 20px;
+            color: #fff;
+        }
+        .login {
+            width: 450px;
+            position: relative;
+            padding: 100px 30px;
+            backdrop-filter: blur(20px);
+            margin-left: auto;
+        }
+        .login h2{
+            font-size: 2em;
+            text-align: center;
+            margin-bottom: 20px;
+            color: #fff;
+        }
+        .login .input{
+            position: relative;
+            width: 100%;
+            height: 30px;
+            margin-bottom: 40px;
+        }
+        .login .input .input1 {
+            font-size: 16px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background: transparent;
+            border:none;
+            outline: none;
+            border-bottom: 2px solid #fff;
+            color: #fff;
+            width: 100%;
+            height: 100%
+        }
 
-            <x-validation-errors class="mb-4" />
+        ::placeholder
+        {
+            color: #fff;
+            font-size: 18px;
+        }
+        
+        .check {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 40px;
+            color: #fff;
+        }
+        .check a {
+            text-decoration: none;
+            color: #fff;
+        }
+        .check a:hover{
+            text-decoration: underline;
+        }
+        .login .button 
+        {
+            width: 100%;
+            height: 40px;
+            margin-bottom: 15px;
+        }
+        button {
+            width: 100%;
+            height: 40px;
+            background-color: crimson;
+            border: none;
+            outline: none;
+            outline: none;
+            outline: none;
+            font-size: 20px;
+            font-weight: 700;
+            border-radius: 7px;
+            color: #fff;
+        }
+        button:active{
+            font-size: 25px;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/c9f5871d83.js" crossorigin="anonymous"></script>
+    <title>Lote.ph</title>
+</head>
+<body>
+    <header class="header">
+        <nav class="nav">
+         
 
-            @if (session('status'))
-                <div class="mb-4 font-medium text-sm text-green-600">
-                    {{ session('status') }}
+            <a class="nav-link" href="/">Home</a>
+
+    </header>
+
+    <div class="background">
+        <section class="home">
+            <div class="content">
+                <a href="#" class="logo">Lote.PH</a>
+                <h2>WELCOME AND INQUIRE</h2>
+                <h3>BUY AND SELL LANDS</h3>
+                <pre> VISIT LOTE.PH FOR LAND LOT INQUIRIES AND SALES</pre>
+                <div class="icon">
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-github"></i>
                 </div>
-            @endif
-
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-
-                <div>
-                    <x-label for="email" value="{{ __('Email') }}" />
-                    <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-                </div>
-
-                <div class="mt-4">
-                    <x-label for="password" value="{{ __('Password') }}" />
-                    <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-                </div>
-
-                <div class="block mt-4">
-                    <label for="remember_me" class="flex items-center">
-                        <x-checkbox id="remember_me" name="remember" />
-                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                    </label>
-                </div>
-
-                <div class="flex items-center justify-end mt-4">
-                    @if (Route::has('password.request'))
-                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                            {{ __('Forgot your password?') }}
-                        </a>
+            </div>
+            <div class="login">
+                <h2>Login</h2>
+                
+                    <x-slot name="logo">
+                        <x-authentication-card-logo />
+                    
+                    
+                    @if (session('status'))
+                    <div class="mb-4 font-medium text-sm text-green-600">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    <x-button class="ml-4">
-                        {{ __('Log in') }}
-                    </x-button>
-                </div>
-            </form>
-        </x-authentication-card>
+                    <form method="POST" action="{{ route('login') }}">
+                        @csrf
+                        <div class="input">
+                            <input id="email" class="input1" type="email" name="email"
+                                :value="old('email')" placeholder="Email" required > 
+                            
+                        </div>
+                        <div class="input">
+                            <input id="password" class="input1" type="password" name="password" placeholder="Password" required
+                                 >
+                            
+                        </div>
+                        <div class="check">
+                            <label for="remember_me" class="flex items-center">
+                                <x-checkbox id="remember_me" name="remember" />
+                                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                            </label>
+                        </div>
+                        <div class="check">
+                            @if (Route::has('password.request'))
+                            <a
+                                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                href="{{ route('password.request') }}">
+                                {{ __('Forgot your password?') }}
+                            </a>
+                            @endif
+                        </div>
+                        <div class="button">
+                            <x-button>
+                                {{ __('Log in') }}
+                            </x-button>
+                        </div>
+                        </div>
+                    </form>
+              
+            </div>
+        </section>
     </div>
-</x-guest-layout>
-
-
+</body>
+</html>
--- resources/views/registration/success.blade.php
+++ resources/views/registration/success.blade.php
@@ -1,17 +1,133 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Registration Success</title>
-    </head>
-    <body>
+
+<head>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+        *
+        {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family: 'Poppins', sans-serif;
+        }
+
+        body
+        {
+            background-image: url('/images/your-background-image.jpg');
+            background-size: cover;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .container {
+            background: rgba(255, 255, 255, 0.8);
+            padding: 20px;
+            border-radius: 5px;
+            width: 400px;
+            text-align: center;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        }
+
+        .container h1 {
+            font-size: 28px;
+            color: #333;
+            margin-bottom: 20px;
+        }
+
+        .container p {
+            font-size: 18px;
+            color: #333;
+        }
+
+        .container .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #007BFF;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            font-size: 18px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+            margin-top: 20px;
+        }
+
+        .container .btn:hover {
+            background-color: #0056b3;
+        }
+
+        .dialog {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: rgba(0, 0, 0, 0.7);
+            justify-content: center;
+            align-items: center;
+            z-index: 1;
+        }
+
+        .dialog-box {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 5px;
+            text-align: center;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        }
+
+        .dialog-box h2 {
+            font-size: 24px;
+            color: #333;
+            margin-bottom: 20px;
+        }
+
+        .dialog-box .btn {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #007BFF;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            font-size: 18px;
+            cursor: pointer;
+            transition: background-color 0.3s ease;
+        }
+
+        .dialog-box .btn:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
         <h1>Registration Successful!</h1>
         <p>Your registration was successful. You can now log in.</p>
-        <div>
-            <ul>
-                <li class="nav-item">
-                    <a class="btn btn-primary" id="logincss" href="{{ route('login') }}">Login</a>
-                 </li>
-            </ul>
+        <a class="btn" id="logincss" href="{{ route('login') }}">Login</a>
+    </div>
+
+    
+    <div class="dialog" id="dialog-box">
+        <div class="dialog-box">
+            <h2>Registration Successful!</h2>
+            <p>Your registration was successful. You can now log in.</p>
+            <a class="btn" id="logincss" href="{{ route('login') }}">Login</a>
         </div>
-    </body>
-    </html>
+    </div>
+
+   
+    <script>
+        setTimeout(function () {
+            document.getElementById("dialog-box").style.display = "flex";
+        }, 2000); 
+    </script>
+</body>
+
+</html>
